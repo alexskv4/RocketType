572c0ae632cf137795e39da6497a63464faa6a7e
alexskv4 <alexskv4@gmail.com>
2022-04-16 14:53:39 -0700

    Added highlighting to the quote while you are typing.

--- src/components/TypingText.svelte
+++ src/components/TypingText.svelte
@@ -1,8 +1,8 @@
 <script>
     import FinishedModal from "./FinishedModal.svelte";
 
-    export let quote = "We all get lost once in a while, sometimes by choice, sometimes due to forces beyond our control. When we learn what it is our soul needs to learn, the path presents itself. Sometimes we see the way out but wander further and deeper despite ourselves; the fear, the anger or the sadness preventing us returning. Sometimes we prefer to be lost and wandering, sometimes it's easier. Sometimes we find our own way out. But regardless, always, we are found."
-    //export let quote = "The quick brown fox jumped over the lazy dog."
+    //export let quote = "We all get lost once in a while, sometimes by choice, sometimes due to forces beyond our control. When we learn what it is our soul needs to learn, the path presents itself. Sometimes we see the way out but wander further and deeper despite ourselves; the fear, the anger or the sadness preventing us returning. Sometimes we prefer to be lost and wandering, sometimes it's easier. Sometimes we find our own way out. But regardless, always, we are found."
+    export let quote = "The quick brown fox jumped over the lazy dog."
     //export let quote = "They don't know that we know they know we know.";
     //export let quote = "abc";
 
@@ -13,12 +13,24 @@
     let wpm;
     let currentText;
     let inputClass= "valid";
+    let validText="";
+    let invalidText="";
+    let untypedText=quote;
 
     function onPopupClose () {
         showPopup = false;
-        currentText = "";
+        reset();
     }
     
+    function reset () {
+
+        currentText = "";
+        validText = "";
+        invalidText = "";
+        untypedText = quote;
+
+    }
+
     function millisecToWpm () {
         console.log(totalTime);
         var secs = totalTime / 1000;
@@ -39,12 +51,34 @@
             showPopup = true;
             
         }
-        else if ( currentText != quote.substring(0, currentText.length)) {
-            inputClass = "invalid";
+        validText = "";
+        invalidText = "";
+        untypedText = quote;
+        for (var i = 0; i < currentText.length; i++) {
+
+            if (currentText.charAt(i) == quote.charAt(i)) {
+                validText += quote.charAt(i);
+                inputClass = "valid";
+            }
+            else {
+                invalidText += quote.substring(i, currentText.length);
+                untypedText = quote.substring(currentText.length )
+                inputClass = "invalid";
+                break;
+            }
+            untypedText = quote.substring(i+1);
         }
-        else {
-            inputClass = "valid";
-        }
+
+
+        // else if ( currentText != quote.substring(0, currentText.length)) {
+        //     inputClass = "invalid";
+        //     invalidText += untypedText.substring(0, 1);
+        // }
+        // else {
+        //     inputClass = "valid";
+        //     validText += untypedText.substring(0, 1);
+        // }
+        // untypedText = untypedText.substring(1);
     }
 
 </script>
@@ -52,7 +86,7 @@
 <main class="container typingBox">
 	<div class="col">
 		<div class="row">
-            <p>{quote}</p>
+            <p><span class="valid">{validText}</span><span class="invalid">{invalidText}</span>{untypedText}</p>
         </div>
 		<div class="row">
             <input class={inputClass} id="typingInput" bind:value={currentText} on:input={(e) => checkValid()}/>
@@ -62,6 +96,16 @@
 <FinishedModal wpm={wpm} open={showPopup} onClosed={() => onPopupClose()}/>
 
 <style>
+    span.invalid {
+        color: red;
+        background-color: pink;
+    }
+    span.valid {
+
+        color: green;
+        background-color: lightgreen;
+
+    }
     input.invalid {
         background-color: coral;
     }
