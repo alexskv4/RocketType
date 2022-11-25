9ef0aa2398b96a63ebb6a002ee35dd324319e30f
alexskv4 <alexskv4@gmail.com>
2022-11-24 19:23:11 -0800

    Formatting time spent typing to hh:mm:ss

--- src/svelte/components/AccountStatsModal.svelte
+++ src/svelte/components/AccountStatsModal.svelte
@@ -6,6 +6,22 @@
     
     export let userStats;
 
+    function padTo2Digits (num) {
+        return num.toString().padStart(2, '0');
+    }
+
+    function formatMilliseconds (ms) {
+        const seconds = Math.floor((ms / 1000) % 60);
+        const minutes = Math.floor((ms / 1000 / 60) % 60);
+        const hours = Math.floor(ms  / 1000 / 3600 );
+
+        const formatted = [
+            padTo2Digits(hours.toString(), 2),
+            padTo2Digits(minutes.toString(), 2),
+            padTo2Digits(seconds.toString(), 2),
+        ].join(':');
+        return formatted;
+    }
 
     const modalClose = () => {
         open = false;
@@ -28,7 +44,7 @@
             <Row><Col><p>Recent average WPM:</p></Col><Col><p>{userStats.recentAverageWpm}</p></Col></Row>
             <Row><Col><p>Recent average accuracy:</p></Col><Col><p>{userStats.averageWpm}</p></Col></Row>
             <Row><Col><p>Races Completed:</p></Col><Col><p>{userStats.racesCompleted}</p></Col></Row>
-            <Row><Col><p>Time spent typing:</p></Col><Col><p>{userStats.timeTyping} convert this to HH mm</p></Col></Row>
+            <Row><Col><p>Time spent typing:</p></Col><Col><p>{formatMilliseconds(userStats.timeTyping)}</p></Col></Row>
         </Col>
     </ModalBody>
     <ModalFooter>
